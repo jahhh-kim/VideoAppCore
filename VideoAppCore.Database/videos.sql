a250c8ae75b59dd85b8791e35c60f65f88b59a7b
jahhh-kim <jaes9930@gmail.com>
2020-09-16 09:43:29 +0900

    MS sql 프로잭트 추가

---
+++ VideoAppCore.Database/videos.sql
@@ -0,0 +1,15 @@
+﻿
+--비디오 테이블
+CREATE TABLE [dbo].[videos]
+(
+	[Id] INT NOT NULL Identity(1,1) Primary Key,
+	Created DateTimeOffset(7) 
+	Default(SysDateTimeOffset() AT TIME ZONE 'korea Standard Time'),
+	[Title] NVarChar(Max) Not Null,  --제목
+	[Url] NVarChar(Max) Null,        --URL
+	 
+	[Name] NVARCHAR(50) NULL,   
+	[Company] NVarChar(255) Null,  --회사
+
+)
+GO
