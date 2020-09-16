c607be6b89223a4b50081c2faa48209bf5ad6c22
jahhh-kim <jaes9930@gmail.com>
2020-09-16 10:15:00 +0900

    API명세서 생성

---
+++ VideoAppCore.Models/IVideoRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoAppCore.Models
+{
+    /// <summary>
+    /// [3] 인터페이스 : videos 테이블에 대한 CRUD API 명세서 작성
+    /// </summary>
+    public interface IVideoRepository
+    {
+        Video AddVedio(Video model); // 입력 : T Add (T model)
+        List<Video> GetVideos();   // 출력 List<T> GetAll()
+        Video GetVideoById(int id);  // 상세 GetById(int id);
+        Video UpdateVideo(Video model); //수정 : T Edit (T model)
+        void RemoveVideo(int id);       //삭제 : void Delet(int id);
+
+        //추가: 검색, 페이징 , 소팅,...
+    }
+}
+
